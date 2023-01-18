3269397c38b23712ebd162bae51e9cbbe263d65f
cegj <cegasparjr@gmail.com>
2023-01-18 12:05:04 -0300

    Create endpoint to config database and standart categories

--- contta-api/app/Http/Controllers/SetupController.php
+++ contta-api/app/Http/Controllers/SetupController.php
@@ -45,12 +45,12 @@ class SetupController extends Controller
 
             if (!databaseExists()){
                 $dbName = getenv('DB_DATABASE'); 
-                return response()->json(["message" => "Não existe banco de dados com o nome informado."], 400);
+                return response()->json(["message" => "Não existe banco de dados com o nome informado. Verifique se o banco de dados foi criado e se tem o mesmo nome infomado nas variáveis de ambiente."], 400);
             }
 
             if (tablesExists()){
                 $dbName = getenv('DB_DATABASE'); 
-                return response()->json(["message" => "Já existem tabelas no banco de dados."], 400);
+                return response()->json(["message" => "Já existem tabelas no banco de dados. Para realizar uma instalação limpa, apague todas as tabelas por meio do software gerenciador de banco de dados."], 400);
             }
 
             Artisan::call('config:clear');
@@ -99,6 +99,46 @@ class SetupController extends Controller
         } catch (\Throwable $th) {
             return response()->json(["message" => "Ocorreu um erro", "error" => $th->getMessage()], 500);
         }
+
+
+    }
+
+    public function createStandardCategories(Request $request){
+        try {
+            $groupsToCreate = [
+                "Automóvel" => ["Combustível", "Estacionamento", "IPVA", "Manutenção veicular", "Multas", "Pedágio", "Seguro veicular", "Outros (Automóvel)"],
+                "Compras" => ["Presentes e doações", "Produtos em geral", "Supermercado", "Vestuário", "Outros (Compras)"],
+                "Dívidas" => ["Cartões de crédito", "Empréstimos", "Financiamentos", "Outros (Dívidas)"],
+                "Educação" => ["Cursos", "Faculdade", "Material escolar", "Outros (Educação)"],
+                "Lazer" => ["Cinema e filmes", "Comida e bebida", "Gastos gerais com viagens", "Hospedagem", "Passagens", "Passeios", "Shows e teatro", "Outros (Lazer)"],
+                "Moradia" => ["Água e gás", "Aluguel", "Condomínio", "Energia elétrica", "IPTU", "Manutenção residencial", "TV e internet", "Outros (Moradia)"],
+                "Pets" => ["Acessórios e brinquedos pet", "Alimentação pet", "Hospedagem pet", "Saúde e higiene pet", "Outros (Pet)"],
+                "Receitas" => ["Salário", "Outros (Receitas)"],
+                "Saúde" => ["Academia e exercícios", "Consultas e exames", "Medicamentos e produtos", "Plano de saúde", "Outros (Saúde)"],
+                "Serviços" => ["Burocracias", "Cabelereiro e estética", "Celular", "Lavanderia", "Seguros diversos", "Serviços online", "Outros (Serviços)"],
+                "Transporte" => ["Transporte público", "Táxi/Uber", "Outros (Transporte)"]];
+    
+            $createdGroups = [];
+
+            foreach ($groupsToCreate as $groupName => $categories){
+                $group = new Group;
+                $group->name = $groupName;
+                $group->save();
+                
+                foreach ($categories as $categoryName){
+                    $category = new Category;
+                    $category->name = $categoryName;
+                    $category->group_id = $group->id;
+                    $category->save();
+                }
+            };
+
+            return response()->json(["message" => "Grupos e categorias criados com sucesso"], 200);
+
+            } catch (\Throwable $th) {
+                return response()->json(["message" => "Ocorreu um erro", "error" => $th->getMessage()], 500);
+            }
+
 
 
     }
--- contta-api/routes/apiRoutes/setupRoutes.php
+++ contta-api/routes/apiRoutes/setupRoutes.php
@@ -4,4 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SetupController;
 
 Route::get('/checktables', [SetupController::class, 'checkTables']);
-Route::get('/database', [SetupController::class, 'setupDatabase']);
+Route::post('/database', [SetupController::class, 'setupDatabase']);
+
+// Protected routes
+Route::group(['middleware' => ['protectedRoute']], function(){
+  Route::post('/categories', [SetupController::class, 'createStandardCategories']);
+});
