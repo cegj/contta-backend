f5d665fbb366dc04f759f97950f779fcd1bafacb
cegj <cegasparjr@gmail.com>
2023-01-29 18:15:32 -0300

    Refactor editBudgetControl on editExpense

--- contta-api/app/Http/Controllers/TransactionController.php
+++ contta-api/app/Http/Controllers/TransactionController.php
@@ -39,8 +39,55 @@ class TransactionController extends Controller
         }
     }
 
-    private function editBudgetControl (){
+    private function editBudgetControl ($transaction, $qBudgetControl, $qPaymentDate, $qCategoryId, $qValue){
 
+        $baseDate = clone $qPaymentDate;
+        $firstDayOfMonth = $baseDate->modify('first day of this month')->format('Y-m-d');
+        $lastDayOfMonth = $baseDate->modify('last day of this month')->format('Y-m-d');
+
+        //O que influencia: VALOR - CATEGORIA - MÊS/ANO(PD)
+        $valueChanged = ($transaction->value != $qValue) ? true : false;
+        $categoryChanged = ($transaction->category_id != $qCategoryId) ? true : false;
+        $paymentDateChanged = (date('Y-m', strtotime($transaction->payment_date)) != date('Y-m', strtotime($qPaymentDate))) ? true : false;
+
+        //Restore value on old/actual budget transaction
+        $baseDate = clone $transaction->payment_date;
+        $firstDayOfMonth = $baseDate->modify('first day of this month')->format('Y-m-d');
+        $lastDayOfMonth = $baseDate->modify('last day of this month')->format('Y-m-d');    
+        $oldBcTransaction = Transaction::whereBetween('payment_date', [$firstDayOfMonth, $lastDayOfMonth])
+        ->where('budget_control', 1)
+        ->where('category_id', $transaction->category_id)
+        ->where('type', $transaction->type)
+        ->get(); 
+        if (count($oldBcTransaction) > 0){
+            $oldBcTransaction = $oldBcTransaction[0];
+            $result = $oldBcTransaction->value + $transaction->value;
+            $budgetControlTransaction->value = $result;
+            $budgetControlTransaction->save();    
+        }
+
+        //Make changes on new BC (wich can be the same or not)
+        $baseDate = clone $qPaymentDate;
+        $firstDayOfMonth = $baseDate->modify('first day of this month')->format('Y-m-d');
+        $lastDayOfMonth = $baseDate->modify('last day of this month')->format('Y-m-d');    
+        $newBcTransaction = Transaction::whereBetween('payment_date', [$firstDayOfMonth, $lastDayOfMonth])
+        ->where('budget_control', 1)
+        ->where('category_id', $qCategoryId)
+        ->where('type', $transaction->type)
+        ->get(); 
+
+        if (count($newBcTransaction) > 0){
+            $newBcTransaction = $newBcTransaction[0];
+            $result = $newBcTransaction->value - $qValue;
+            if ($result > 0) {
+                $exceedValue = number_format(floatval($result)/100, 2, ",", ".");
+                throw new \Exception("O valor desta transação ultrapassa o limite do controle do orçamento de {$newBcTransaction->transaction_date} em R$ {$exceedValue}", 400); 
+            } else {
+                $newBcTransaction->value = $result;
+                $newBcTransaction->save();    
+            }
+        }
+        return;
     }
 
     public function getTransactions(Request $request){
@@ -1007,6 +1054,7 @@ class TransactionController extends Controller
     
                     $i = $ref_transaction->installment - 1;
                     while($i < $total_installments){
+
                         $baseDate = clone $payment_date;
                         $firstDayOfMonth = $baseDate->modify('first day of this month')->format('Y-m-d');
                         $lastDayOfMonth = $baseDate->modify('last day of this month')->format('Y-m-d');
@@ -1023,23 +1071,7 @@ class TransactionController extends Controller
                         } 
         
                         if (!$transactions[$i]->budget_control && !$budget_control){
-                            $budgetControlTransaction = Transaction::whereBetween('payment_date', [$firstDayOfMonth, $lastDayOfMonth])
-                            ->where('budget_control', 1)
-                            ->where('category_id', $category_id)
-                            ->where('type', 'D')
-                            ->get(); 
-            
-                            if (count($budgetControlTransaction) > 0){
-                                $budgetControlTransaction = $budgetControlTransaction[0];
-                                $result = ($budgetControlTransaction->value + $transactions[$i]->value) - $value;
-                                if ($result > 0) {
-                                    $exceedValue = number_format(floatval($result)/100, 2, ",", ".");
-                                    throw new \Exception("O valor desta transação ultrapassa o limite do controle do orçamento de {$budgetControlTransaction->transaction_date} em R$ {$exceedValue}", 400); 
-                                } else {
-                                    $budgetControlTransaction->value = $result;
-                                    $budgetControlTransaction->save();    
-                                }
-                            }
+                            $this->editBudgetControl($transactions[$i], $budget_control, $payment_date, $category, $value);
                         }
 
                         $transactions[$i]->transaction_date = $transactionDateStr;
